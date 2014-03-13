26404d1fa52ac3be2ccfc2f2d4e252acd2e342e3
QueryInterface <volkov.stanislav@gmail.com>
2014-03-13 20:17:33 +0400

    Error description added in message box

--- Utils/ErrorHandler.cpp
+++ Utils/ErrorHandler.cpp
@@ -2,6 +2,7 @@
 #include "common.h"
 #include <stdio.h>
 #include <stdexcept>
+#include <sstream>
 
 #define MAX_MESSAGE_SIZE 512
 ErrorHandler::ErrorHandler() {
@@ -9,10 +10,12 @@ ErrorHandler::ErrorHandler() {
 
 void ErrorHandler::Process(ERROR_TYPE errorType, char* component, char* file, int line, wchar_t* format, ...) {
     wchar_t message[MAX_MESSAGE_SIZE];
+    std::wstringstream messageStream;
     va_list args;
     va_start(args, format);
     vswprintf(message, format, args);
-    ::MessageBoxW(NULL, message, L"Error", NULL);
+    messageStream << "(" << file << ": " << line << ") " << message;
+    ::MessageBoxW(NULL, messageStream.str().c_str(), L"Error", NULL);
     throw std::exception("");
     va_end(args);
 }
