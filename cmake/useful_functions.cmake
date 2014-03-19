23cf5b069327aa945f59c863979a589573a8f8f0
QueryInterface <volkov.stanislav@gmail.com>
2014-03-19 19:11:16 +0400

    Platform detection for qt in cmake

--- cmake/useful_functions.cmake
+++ cmake/useful_functions.cmake
@@ -34,3 +34,26 @@ macro(update_compiler_settings)
 	SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -WX")
   endif()
 endmacro()
+
+# Check if we compile for iphone
+macro(check_apple_platform)
+  if(APPLE)
+    include(CheckCSourceCompiles)
+    check_c_source_compiles("
+      #include \"TargetConditionals.h\"
+      int main(int argc, char *argv[]) {
+      #if !TARGET_OS_MAC
+        -1
+      #endif
+      }
+    " OS_OSX)
+    check_c_source_compiles("
+      #include \"TargetConditionals.h\"
+      int main(int argc, char *argv[]) {
+      #if !TARGET_OS_IPHONE
+        -1
+      #endif
+      }
+    " OS_IPHONE)
+  endif()
+endmacro()
