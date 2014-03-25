3cb74fe07f7fae0ee7729fbbd0c391c0c11acf73
pgolovin <pavel.golovinskiy@gmail.com>
2014-03-26 00:49:34 +0300

    reduced version of QuadTree added

--- Utils/include/QuadTree.h
+++ Utils/include/QuadTree.h
@@ -1,14 +1,107 @@
 #pragma once
-#include <stdint.h>
-#include <map>
+#include <list>
+#include <memory>
 
 namespace Utils
 {
+    // This quad tree is highly limited by it's usage
+    // it works with square areas, the side is pow2
 
     template <class T>
     class QuadTree
     {
+    public:
+        QuadTree(size_t squareSide) : m_squareSide(squareSide) {}
 
+        void insert(size_t x, size_t y, T value)
+        {
+            Node* node      = & m_root;
+            size_t width    = m_squareSide;
+
+            while (width > 1)
+            {
+                width >>= 1;
+
+                // !!(A) convert any non-null number to 1
+                size_t index = (!!(x & width)) + ((!!(y & width)) << 1);
+                x &= ~width;
+                y &= ~width;
+
+                if ( !node->quadNodes[index] )
+                {
+                    node->quadNodes[index].reset(new Node());
+                }
+                node = node->quadNodes[index].get();
+            }
+            node->value = value;
+        }
+
+        const T* item_at(size_t x, size_t y)
+        {
+            Node* node      = & m_root;
+            size_t width    = m_squareSide;
+
+            while (width > 1)
+            {
+                width >>= 1;
+
+                // !!(A) convert any non-null number to 1
+                size_t index = (!!(x & width)) + ((!!(y & width)) << 1);
+                x &= ~width;
+                y &= ~width;
+
+                if ( !node->quadNodes[index] )
+                {
+                    return nullptr;
+                }
+                node = node->quadNodes[index].get();
+            }
+            return &node->value;
+        }
+
+        void remove(size_t x, size_t y)
+        {
+            Node* node      = & m_root;
+            size_t width    = m_squareSide;
+            Node* lastFull  = & m_root;
+            size_t targetIndex    = (!!(x & (width >> 1))) + ((!!(y & (width >> 1))) << 1);
+
+            while (width > 1)
+            {
+                width >>= 1;
+
+                // !!(A) convert any non-null number to 1
+                size_t index = (!!(x & width)) + ((!!(y & width)) << 1);
+                x &= ~width;
+                y &= ~width;
+
+                if ( !node->quadNodes[index] )
+                {
+                    return;
+                }
+                //Get number of non null nodes
+                if (((!!node->quadNodes[0]) + (!!node->quadNodes[1]) + (!!node->quadNodes[2]) + (!!node->quadNodes[3])) > 1)
+                {
+                    lastFull = node;
+                    targetIndex = index;
+                }
+                node = node->quadNodes[index].get();
+                
+            }
+            std::unique_ptr<Node> toRemove(std::move(lastFull->quadNodes[targetIndex]));
+            toRemove.reset();
+        }
+    private:
+        struct Node
+        {
+            // | 0 | 1 |
+            // | 2 | 3 |
+            std::unique_ptr<Node> quadNodes[4];
+            T value;
+        };
+
+        size_t m_squareSide;
+        Node   m_root;
     };
 
 }
--- Utils/test/QuadTreeTest.cpp
+++ Utils/test/QuadTreeTest.cpp
@@ -1,6 +1,7 @@
 #include "QuadTree.h"
 #include <gtest/gtest.h>
 #include <memory>
+#include <map>
 
 using namespace Utils;
 
@@ -10,7 +11,7 @@ public:
 
     void SetUp()
     {
-        m_tree.reset(new QuadTree<int>());
+        m_tree.reset(new QuadTree<int>(512));
     }
     void TearDown()
     {
@@ -20,5 +21,122 @@ protected:
     std::unique_ptr< QuadTree<int> > m_tree;
 };
 
+TEST_F(QuadTreeTest, EmptyItem)
+{
+    ASSERT_TRUE( nullptr == m_tree->item_at(254, 400) );
+}
 
+TEST_F(QuadTreeTest, Insert)
+{
+    ASSERT_NO_THROW( m_tree->insert(254, 400, 10) );
+}
+
+TEST_F(QuadTreeTest, InsertedItem)
+{
+    ASSERT_NO_THROW( m_tree->insert(254, 400, 10) );
+    ASSERT_TRUE( nullptr == m_tree->item_at(400, 254) );
+    ASSERT_EQ(10, *m_tree->item_at(254, 400));
+}
+
+TEST_F(QuadTreeTest, RewriteItem)
+{
+    m_tree->insert(254, 400, 10);
+    ASSERT_EQ(10, *m_tree->item_at(254, 400));
+    ASSERT_EQ(10, *m_tree->item_at(254, 400));
+    m_tree->insert(254, 400, 50);
+    ASSERT_EQ(50, *m_tree->item_at(254, 400));
+}
+
+TEST_F(QuadTreeTest, RemoveItem)
+{
+    m_tree->insert(254, 400, 10);
+    ASSERT_EQ(10, *m_tree->item_at(254, 400));
+    ASSERT_NO_THROW( m_tree->remove(254, 400) );
+    ASSERT_TRUE( nullptr == m_tree->item_at(254, 400) );
+}
+
+TEST_F(QuadTreeTest, RemoveItems)
+{
+    m_tree->insert(254, 400, 10);
+    m_tree->insert(254, 401, 15);
+    ASSERT_NO_THROW( m_tree->remove(254, 400) );
+    ASSERT_TRUE( nullptr == m_tree->item_at(254, 400) );
+    ASSERT_TRUE( nullptr != m_tree->item_at(254, 401) );
+    ASSERT_EQ( 15, *m_tree->item_at(254, 401) );
+}
+
+class QuadTreeBenchmark : public ::testing::Test
+{
+     
+public:
+
+    static void SetUpTestCase()
+    {
+        m_tree.reset(new QuadTree<int>(512));
+        x.resize(iterrations);
+        y.resize(iterrations);
+    }
+    static void TearDownTestCase()
+    {
+        m_tree.reset();
+    }
+protected:
+    static const size_t iterrations = 1000000;
+    static std::vector<size_t> x;
+    static std::vector<size_t> y;
+    static std::map<int, int> v;
+    static std::unique_ptr< QuadTree<int> > m_tree;
+};
+
+std::vector<size_t> QuadTreeBenchmark::x;
+std::vector<size_t> QuadTreeBenchmark::y;
+std::map<int, int> QuadTreeBenchmark::v;
+std::unique_ptr< QuadTree<int> > QuadTreeBenchmark::m_tree;
+
+TEST_F(QuadTreeBenchmark, InsertPerformance)
+{
+    for (unsigned int i = 0; i < iterrations; ++i)
+    {
+        size_t X= rand() % 512;
+        size_t Y = rand() % 512;
+        int V = rand() % 512;
+        x[i] = X;
+        y[i] = Y;
+        m_tree->insert(X, Y, V);
+    }
+}
+
+TEST_F(QuadTreeBenchmark, AccessPerformance)
+{
+    for (unsigned int i = 0; i < iterrations; ++i)
+    {
+        m_tree->item_at(x[i], y[i]);
+    }
+}
+
+TEST_F(QuadTreeBenchmark, RemovePerformance)
+{
+    for (unsigned int i = 0; i < iterrations; ++i)
+    {
+        m_tree->remove(x[i], y[i]);
+    }
+}
+
+/*TEST_F(QuadTreeBenchmark, QuadTreeCorrectness)
+{
+    for (unsigned int i = 0; i < iterrations; ++i)
+    {
+        size_t X= rand() % 512;
+        size_t Y = rand() % 512;
+        int V = rand() % 512;
+        x[i] = X;
+        y[i] = Y;
+        v[X | (Y << 8)] = V;
+        m_tree->insert(X, Y, V);
+    }
+    for (unsigned int i = 0; i < iterrations; ++i)
+    {
+        ASSERT_EQ(v[x[i] | (y[i] << 8)], *m_tree->item_at(x[i], y[i])) << " broken at " << i;
+    }
+}*/
 // eof
