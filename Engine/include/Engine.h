370a2f33d52492f8f7be18faa4f884e847c323b8
QueryInterface <volkov.stanislav@gmail.com>
2014-04-13 14:34:57 +0400

    ISceneElement interface

--- Engine/include/Engine.h
+++ Engine/include/Engine.h
@@ -20,7 +20,7 @@ struct IScene;
 struct IResourceOverseer;
 struct IEngine;
 
-typedef shared_ptr<ISceneElement>        ISceneElementPtr;
+typedef shared_ptr<ISceneElement>       ISceneElementPtr;
 typedef shared_ptr<IMesh>               IMeshPtr;
 typedef shared_ptr<ITexture>            ITexturePtr;
 typedef shared_ptr<IProgram>            IProgramPtr;
@@ -32,23 +32,33 @@ typedef shared_ptr<ILight>              ILightPtr;
 typedef shared_ptr<IResourceOverseer>   IResourceOverseerPtr;
 typedef shared_ptr<IEngine>             IEnginePtr;
 
-struct IClonable  {
+struct IClonable  
+{
     virtual IClonable* Clone() = 0;
 };
 
 // Specifies elements that could be added to scene graph
-struct ISceneElement {
+// There are a few ways to attach object:
+// 1. Bidirectional attachment (rigid connection) - move of one object causes move of another one
+// 2. Directiona attachmant - A->B move of object A causes move of object B, move of object B doesn't cause move of object A 
+struct ISceneElement 
+{
+    virtual void AttachBidirectional(ISceneElementPtr sceneElement) = 0;
+    virtual void AttachDirectional(ISceneElementPtr sceneElement)   = 0;
 };
 
 // resource interfaces
-struct IResource : public IClonable {
+struct IResource : public IClonable 
+{
 };
 
-struct IMesh : public IResource {
+struct IMesh : public IResource 
+{
     // tbd
 };
 
-struct ITexture : public IResource {
+struct ITexture : public IResource 
+{
     // tbd
 };
 
@@ -56,17 +66,20 @@ struct ISceleton : public IResource {
     // tbd
 };
 
-struct IProgram : public IResource {
+struct IProgram : public IResource 
+{
     virtual string  GetVertexShader() const             = 0;
     virtual string  GetPixelShader() const              = 0;
     virtual void    SetVertexShader(const string& path) = 0;
     virtual void    SetPixelShader(const string& path)  = 0;
 };
 
-struct IScript : public IResource {
+struct IScript : public IResource 
+{
 };
 
-struct ICamera : public ISceneElement {
+struct ICamera : public ISceneElement 
+{
 
 };
 
@@ -96,18 +109,22 @@ struct IObject
 // scene interfaces
 struct ILight 
     : public IClonable
-    , public ISceneElement {
+    , public ISceneElement 
+{
     // Sets
     virtual void            SetPosition(Vector3<float> pos) = 0;
     // Gets
     virtual Vector3<float>  GetPosition() const             = 0;    
 };
 
-struct IScene {
-    virtual void AddSceneObject(ISceneElementPtr obj) = 0;
+// Base level of all objects
+struct IScene
+{
+    virtual void AddSceneElement(ISceneElementPtr obj) = 0;
 };
 
-struct IResourceOverseer {
+struct IResourceOverseer 
+{
     virtual IMeshPtr        CreateMesh(const wstring& path)                 = 0;
     virtual ITexturePtr     CreateTexture(const wstring& path)              = 0;
     virtual IScriptPtr      CreateScript(const wstring& path)               = 0;
@@ -115,7 +132,8 @@ struct IResourceOverseer {
     virtual IObjectPtr      CreateObject(IMeshPtr mesh, ITexturePtr texture)= 0;
 };
 
-struct IEngine : public IHandle {
+struct IEngine : public IHandle 
+{
     // Sets
     virtual void                    SetScene(IScenePtr scene)   = 0;
     // Gets
